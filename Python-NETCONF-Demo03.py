148e11d8a1d2dc7df0fde6bcd5388708bbce732a
DavidJohnGee <david.gee@ipengineer.net>
2015-03-30 22:14:16 +0100

    Tidied content up for Demo03 and XMl content

--- Python-NETCONF-Demo03.py
+++ Python-NETCONF-Demo03.py
@@ -1,19 +1,34 @@
-#! /usr/bin/Python
+#! /usr/bin/python
 
 from lxml import etree
+
+#import ietfipspace Python file
 import ietfipspace
 
-rootObject = ietfipspace.parse('ipspaceoutput.xml')
+ifacetype = ""
 
-print "===== Print DIR on rootObject ====="
-print dir(rootObject)
+print "===== XML Document Loaded ====="
+rootObject = ietfipspace.parse('ipspaceoutput2.xml')
 
-print "===== Print host name currently set ====="
+#print "===== Print DIR on rootObject ====="
+#print dir(rootObject)
+
+print "\n\n===== Host name currently set ====="
 print rootObject.get_host_name()
 
-print "===== Set hostname and print ====="
-rootObject.set_host_name("Test01")
-print rootObject.get_host_name()
-print "\n\n\n"
+# Get Interfaces list from the rootObject
+interfaces = rootObject.get_interface()
 
-rootObj, rootElement, mapping, reverse_mapping = ietfipspace.parseEtree('ipspaceoutput.xml')
+print "\n\n===== Print settings for each interface ====="
+#Iterate through them and print off detail
+for interface in interfaces:
+    print "Interface name is:\t\t" + interface.get_name()
+    print "Interface description is:\t" + interface.get_description()
+    print "Interface MTU is:\t\t" + str(interface.get_mtu())
+
+    if interface.GigEthernet:
+        ifacetype = "Gigabit Ethernet"
+    if interface.FastEthernet:
+        ifacetype = "FastEthernet"
+        
+    print "Interface type is:\t\t" + ifacetype
